2afa594b65a7dcb1eda3f72834d45fff269af33e
Prathyusha Madabhushi <Prathysusha.Madabhushi@vml.com>
2017-01-09 09:25:07 -0600

    update

---
+++ scripts/js/typescript.js
@@ -0,0 +1,5 @@
+/**
+ * Created by pathysusahmadabhashi on 1/6/17.
+ */
+var message = 'Hey typescript';
+console.log(message);
--- scripts/ts/index.ts
+++ scripts/ts/index.ts
@@ -1,3 +1,5 @@
 /**
  * Created by pathysusahmadabhashi on 1/6/17.
  */
+let message = 'Hey typescript';
+console.log(message);
